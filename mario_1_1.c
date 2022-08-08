a7e1473751a01ad83ebc26100d193a3fc87e02be
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-08-08 20:56:44 +0200

    Fix styling issues

--- mario_1_1.c
+++ mario_1_1.c
@@ -1,41 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-const char HASH = '#';
-const char WHITE = ' ';
-int n;
-    do
-    {
+int main(void) {
+
+    const char HASH = '#';
+    const char WHITE = ' ';
+    int n;
+
+    do {
         n = get_int("Height: ");
     }
     while (n < 1 || n > 8);
     
-        for (int i = 0; i < n; i++)
-
-        {
-            int k = (n - i);
-
-            do
-            {
-                printf("%c", WHITE);
-                k--;
-
-            }
-
-            while (k >=0);
-
-
-            int a = 0;
-            do
-            {
-                printf("%c", HASH);
-                a++;
-            }
-            while (a <= i);
-            printf("\n");
+    for (int i = 0; i < n; i++) {
+        int k = (n - i);
+        do {
+            printf("%c", WHITE);
+            k--;
         }
+        while (k >=0);
+        
+        int a = 0;
 
+        do {
+            printf("%c", HASH);
+            a++;
+        }
+        while (a <= i);
 
+        printf("\n");
+    }
 }
