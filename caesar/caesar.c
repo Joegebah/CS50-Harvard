c5552bb980959d6c80894ea04c346d8ee6b5465e
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-08-08 20:38:11 +0200

    Fix logic issues and styling issues

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,72 +3,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-char ALPHABET_LOWER[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char ALPHABET_UPPER[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',  'X', 'Y', 'Z'};
+bool only_digits(string s);
+char rotate(char c, int i);
 
-int main(int argc, string argv[])  {
+int main(int argc, string argv[]) {
 
-    bool isValidInput = argc != 2;
-    if (isValidInput) {
+    bool isNotValidInput = argc != 2;
+
+    if (isNotValidInput) {
         printf("Usage: Please enter a single number as a key. Like this: ./caesar key\n");
 
         return 1;
     }
+    if (only_digits(argv[1])) {
 
-    int key = atoi(argv[1]) % 26;
+        int key = atoi(argv[1]) % 26;
+        string plain = get_string("plaintext: ");
 
-    if (key < 1) {
-        printf("Usage: Please enter a single number as a key. Like this: ./caesar key\n");
+        for (int i = 0; i < strlen(plain); i++) {
+            printf("%c", rotate(plain[i], key));
+        }
+
+        return 0;
+    }
+    else {
+        printf("Usage: ./caesar key");
 
         return 1;
     }
+}
 
-    string plain = get_string("plaintext: "); 
+bool only_digits(string s) {
+    for (int i = 0; i < strlen(s); i++) {
 
-    for (int i = 0; i < strlen(argv[1]); i++) {
-        if (argv[1][i] < 'a' && argv[1][i] > 'z' || argv[1][i] < 'A' && argv[1][i] > 'Z') {
-            printf("Usage: Please enter a single number higher than 0 as a key. Like this: ./caesar key\n");
+        bool hasOnlyDigits = s[i] >= '0' && s[i] <= '9';
 
-            return 1;
+        if (hasOnlyDigits) return true;
+    }
+    return false;
+}
+
+char rotate(char c, int i) {
+
+    bool isInLowerAlphabet = c >= 'a' && c <= 'z';
+    bool isInUpperAlphabet = c >= 'A' && c <= 'Z';
+    
+    if (isInLowerAlphabet) {
+        for (int j = 1; j <= i; j++) {
+            c = c + 1;
+            if (c == 'z' + 1) {
+                c = 'a' + (i - j);
+
+                return c;
+            }
+        }   
+
+        return c;
+    }
+    else if (isInUpperAlphabet) {
+        for (int j = 1; j <= i; j++) {
+            c = c + 1;
+            if(c == 'Z' + 1) {
+                c = 'A' + (i - j);
+
+                return c;
+            }
         }
+
+        return c;
     }
 
-    for (int i = 0; i < strlen(plain); i++) {
-        for (int j = 0; j < sizeof(ALPHABET_LOWER); j++) {
-
-            bool isInAlphabets = plain[i] == ALPHABET_LOWER[j] || plain[i] == ALPHABET_UPPER[j];
-            bool isNotInAlphabets = plain[i] < 'a' && plain[i] > 'z' || plain[i] < 'A' && plain[i] > 'Z';
-            int overflow;
-            bool overflowIsFixed = false;
-
-            if (isInAlphabets) {
-                plain[i] = plain[i] + key;
-                
-                break;
-            }
-            
-            if ((plain[i] + key) > 'z') {
-                overflow = 0;
-                while (isInAlphabets) {
-                plain[i] = plain[i] + key;
-                overflow++;
-                }
-                plain[i] = 'a' + (key - overflow);
-                break;
-                
-            }
-            else if ((plain[i] + key) > 'Z') {
-                overflow = 0;
-                while (isInAlphabets) {
-                plain[i] = plain[i] + key;
-                overflow++;
-                }
-                plain[i] = 'A' + (key - overflow);
-                break;
-            }
-        }
-    }   
-    printf("ciphertext:  %s\n", plain);
-
-    return 0;
+    return c;
 }
