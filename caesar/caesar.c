388bd641aaaa87c48e781f0b95f78b1320805847
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-08-10 16:25:39 +0200

    Fix styling issues

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,8 +6,8 @@
 bool only_digits(string s);
 char rotate(char c, int i);
 
-int main(int argc, string argv[]) {
-
+int main(int argc, string argv[]) { 
+    
     bool isNotValidInput = argc != 2;
 
     if (isNotValidInput) {
@@ -15,8 +15,8 @@ int main(int argc, string argv[]) {
 
         return 1;
     }
-    if (only_digits(argv[1])) {
 
+    if (only_digits(argv[1])) {
         int key = atoi(argv[1]) % 26;
         string plain = get_string("plaintext: ");
         
@@ -36,7 +36,6 @@ int main(int argc, string argv[]) {
 
 bool only_digits(string s) {
     for (int i = 0; i < strlen(s); i++) {
-
         bool hasOnlyDigits = s[i] >= '0' && s[i] <= '9';
 
         if (hasOnlyDigits) return true;
@@ -65,7 +64,7 @@ char rotate(char c, int i) {
     else if (isInUpperAlphabet) {
         for (int j = 1; j <= i; j++) {
             c = c + 1;
-            if(c == 'Z' + 1) {
+            if (c == 'Z' + 1) {
                 c = 'A' + (i - j);
 
                 return c;
