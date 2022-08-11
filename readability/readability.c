db0ffbd27ed36479c66ce1f94a3c7bceaa5fc899
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-08-11 09:28:51 +0200

    Fix styling ussues

--- readability/readability.c
+++ readability/readability.c
@@ -17,20 +17,20 @@ int main(void) {
     index = round(0.0588 * ((letters / words) * 100.0) - (0.296 * ((sentences / words) * 100.0)) - 15.8);
 
     if (index > 16) {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
     else if (index < 1) {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
     else {
-        printf("Grade %0.f", index);
+        printf("Grade %0.f\n", index);
     }
 }
 
-int count_letters(string s) {
+int count_letters(string text) {
     int numberOfLetters = 0;
-    for (int i = 0; i < strlen(s); i++) {
-        if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z') {
+    for (int i = 0; i < strlen(text); i++) {
+        if (text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z') {
             numberOfLetters++;
         }
     }
@@ -38,17 +38,17 @@ int count_letters(string s) {
     return numberOfLetters;
 }
 
-int count_words(string s) {
+int count_words(string text) {
     int numberOfWords;
-    if (s[0] >= 'a' && s[0] <= 'z' || s[0] >= 'A' && s[0] <= 'Z') {
+    if (text[0] >= 'a' && text[0] <= 'z' || text[0] >= 'A' && text[0] <= 'Z') {
         numberOfWords = 1;
     }
     else {
         numberOfWords = 0;
     }
     
-    for (int i = 0; i < strlen(s); i++) {
-        if (s[i] == ' ') {
+    for (int i = 0; i < strlen(text); i++) {
+        if (text[i] == ' ') {
             numberOfWords++;
         }
     }
@@ -56,10 +56,10 @@ int count_words(string s) {
     return numberOfWords;
 }
 
-int count_sentences(string s) {
+int count_sentences(string text) {
     int numberOfSentences = 0;
-    for (int i = 0; i < strlen(s); i++) {
-        if (s[i] == '.' || s[i] == '!' || s[i] == '?') {
+    for (int i = 0; i < strlen(text); i++) {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
             numberOfSentences++;
         }
     }
