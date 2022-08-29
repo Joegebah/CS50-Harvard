458a126a4ed9937c2ce513a804fd2bf1d171c12a
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-08-29 13:01:28 +0200

    Remove all comments

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -1,55 +1,37 @@
-// Implements a dictionary's functionality
-
 #include <ctype.h>
 #include <stdbool.h>
-
 #include "dictionary.h"
 
-// Represents a node in a hash table
-typedef struct node
-{
+typedef struct node {
     char word[LENGTH + 1];
     struct node *next;
 }
 node;
 
-// TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
-
-// Hash table
 node *table[N];
 
-// Returns true if word is in dictionary, else false
-bool check(const char *word)
-{
-    // TODO
+bool check(const char *word) {
+    
     return false;
 }
 
-// Hashes word to a number
-unsigned int hash(const char *word)
-{
-    // TODO: Improve this hash function
+unsigned int hash(const char *word) {
+
     return toupper(word[0]) - 'A';
 }
 
-// Loads dictionary into memory, returning true if successful, else false
-bool load(const char *dictionary)
-{
-    // TODO
+bool load(const char *dictionary) {
+    
     return false;
 }
 
-// Returns number of words in dictionary if loaded, else 0 if not yet loaded
-unsigned int size(void)
-{
-    // TODO
+unsigned int size(void) {
+    
     return 0;
 }
 
-// Unloads dictionary from memory, returning true if successful, else false
-bool unload(void)
-{
-    // TODO
+bool unload(void) {
+    
     return false;
 }
