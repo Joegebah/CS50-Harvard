3455814537bc6a956619ffee6e31fc90a67f8e6f
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-09-02 10:09:37 +0200

    Fix styling issues

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -10,7 +10,7 @@ typedef int16_t DOUBLE_BYTE;
 int main(int argc, char *argv[]) {
     if (argc != 4) {
         printf("Usage: ./volume input.wav output.wav factor\n");
-        
+
         return 1;
     }
 
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
         buffer = buffer * factor;
         fwrite(&buffer, sizeof(DOUBLE_BYTE), 1, output);
     }
-
+    
     fclose(input);
     fclose(output);
 }
