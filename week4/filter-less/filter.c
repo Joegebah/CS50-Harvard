5a1c3066262fcf2ab2ba8d88dae4fd47e2fc6a30
AlenoJankowsky <aleno.jankwosky@5minds.de>
2022-09-02 09:57:00 +0200

    Fix styling issues

--- week4/filter-less/filter.c
+++ week4/filter-less/filter.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[]) {
         printf("Not enough memory to store image.\n");
         fclose(outptr);
         fclose(inptr);
-        
+
         return 7;
     }
 
@@ -106,5 +106,6 @@ int main(int argc, char *argv[]) {
 
     fclose(inptr);
     fclose(outptr);
+    
     return 0;
 }
