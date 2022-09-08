b62ed1960a62f2d8b24be1434709a79c8217e650
Aleno Jankowsky <alenojankowsky@MatthiasMacBook.office.5minds.de>
2022-09-08 13:59:03 +0200

    Add solution for sentimental-credit

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -2,23 +2,23 @@ import re
 
 def main():
     credit_card_is_valid = False
-    individual_digit_of_product_sum = 0
-    sum_of_not_multiplier = 0
     while not credit_card_is_valid:
+        individual_digit_of_product_sum = 0
+        sum_of_not_multiplier = 0
         credit_card_number = int(input("please enter your credit card number:  "))
         credit_card_number_copy = credit_card_number
         credit_card_number_copy = int(credit_card_number_copy / 10)
     
         while True:
-            product_sum_lesser_ten = ((credit_card_number_copy  % 10) * 2) < 10
-            if product_sum_lesser_ten :
+            if ((credit_card_number_copy  % 10) * 2) < 10 :
                 individual_digit_of_product_sum = individual_digit_of_product_sum + (credit_card_number_copy % 10) * 2
             else: 
-                individual_digit_of_product_sum = individual_digit_of_product_sum + (((credit_card_number_copy % 10) * 2) % 10) + ((((credit_card_number_copy % 10) * 2) / 10) % 10)
-                
-            credit_card_number_copy = int(credit_card_number_copy / 100)
-            if not credit_card_number_copy > 0 or (int(credit_card_number_copy) / 10) > 10:
+                individual_digit_of_product_sum = individual_digit_of_product_sum + (((credit_card_number_copy % 10) * 2) % 10) + int((((credit_card_number_copy % 10) * 2) / 10)) % 10
+
+            if not credit_card_number_copy >= 0 or not (int(credit_card_number_copy) / 10) >= 10:
                 break
+
+            credit_card_number_copy = int(credit_card_number_copy / 100)
 
         credit_card_number_copy = credit_card_number
 
@@ -26,13 +26,11 @@ def main():
             sum_of_not_multiplier = sum_of_not_multiplier + (credit_card_number_copy % 10)
             credit_card_number_copy = int(credit_card_number_copy / 100)
 
-            if not (credit_card_number_copy > 0 or (int(credit_card_number_copy / 100)) >= 10):
+            if not (credit_card_number_copy > 0 or (int(credit_card_number_copy / 100)) > 10):
                 break
 
         check = sum_of_not_multiplier + individual_digit_of_product_sum
-        print(check)
         check_is_valid = (check % 10) == 0
-        print(check_is_valid)
         credit_card_number = str(credit_card_number)
 
         if check_is_valid:
@@ -43,6 +41,7 @@ def main():
                     credit_card_is_valid = True
                 else:
                     print("Unknown credit id, ")
+
             elif (len(credit_card_number) == 16):
                 is_master = re.findall("^51", credit_card_number) or re.findall("^52", credit_card_number) or re.findall("^53", credit_card_number) or re.findall("^54", credit_card_number) or re.findall("^55", credit_card_number)
                 is_visa = re.findall("^4", credit_card_number)
@@ -54,6 +53,7 @@ def main():
                     credit_card_is_valid = True
                 else:
                     print("Unknown credit id, ")
+
             elif (len(credit_card_number) == 13 or len(credit_card_number) == 14):
                 is_visa = re.findall("^4", credit_card_number)
                 if is_visa:
@@ -61,6 +61,7 @@ def main():
                     credit_card_is_valid = True
                 else:
                     print("Unknown credit id, ")
+                    
         else:
             print("INVALID")
 
