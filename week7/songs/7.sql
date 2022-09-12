58b9ea639fc0d9e19a8c32d418edf059ff8a9bc1
Aleno Jankowsky <alenojankowsky@MatthiasMacBook.office.5minds.de>
2022-09-12 13:29:14 +0200

    Update code style

--- week7/songs/7.sql
+++ week7/songs/7.sql
@@ -1,1 +1,4 @@
-SELECT AVG(energy) FROM songs INNER JOIN artists ON artist_id = artists.id WHERE artists.name = "Drake";
+SELECT AVG(energy) 
+  FROM songs 
+    INNER JOIN artists ON artist_id = artists.id 
+ WHERE artists.name = "Drake";
